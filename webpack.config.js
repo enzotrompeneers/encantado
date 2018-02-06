4fe95cab7fff54baaa20eb94f560d017a3c0dd04
enzotrompeneers <trompeneers@telenet.be>
2018-02-06 12:58:24 +0100

    init webpack, init foundation, section header

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+path = require('path');
+webpack = require('webpack');
+ExtractTextPlugin = require('extract-text-webpack-plugin');
+extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css',
+});
+
+module.exports = {
+    entry: './src/js/app.js', 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new webpack.optimize.UglifyJsPlugin({
+            // ...
+        })
+    ]
+};
