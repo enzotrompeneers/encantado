f19d520b5f297a71ee25600e699b3bffa5fa5fa2
enzotrompeneers <trompeneers@telenet.be>
2018-02-13 09:02:56 +0100

    contact section with multi form and footer section

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,14 @@
 import 'jquery';
 import '../css/main.scss';
+
+// START: multi form
+$('form.multi-form a.next').click(function() {
+    $('fieldset.active').removeClass('active')
+    .next('fieldset').addClass('active');
+  });
+  
+  $('form.multi-form a.prev').click(function() {
+    $('fieldset.active').removeClass('active')
+    .prev('fieldset').addClass('active');
+  })
+// END: multi form
