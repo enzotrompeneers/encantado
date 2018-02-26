0a4cf2d8adfe1627da62c3908dade89d80be47a6
enzotrompeneers <trompeneers@telenet.be>
2018-02-26 08:59:38 +0100

    front end checklist: direction lecture, social meta, twitter card, html5 semantic elements, w3s validator and dirty markup

--- src/js/app.js
+++ src/js/app.js
@@ -4,36 +4,29 @@ import foundation from 'foundation-sites';
 import ajaxForm from 'jquery-form';
 import '../css/style.scss';
 //require.context('../graphics/', true, /^\.\//);
-
 // START: multi form
 $('form.multi-form a.next').click(function() {
-    $('fieldset.active').removeClass('active')
-    .next('fieldset').addClass('active');
-  });
-  
-  $('form.multi-form a.prev').click(function() {
-    $('fieldset.active').removeClass('active')
-    .prev('fieldset').addClass('active');
-  })
+	$('fieldset.active').removeClass('active').next('fieldset').addClass('active');
+});
+$('form.multi-form a.prev').click(function() {
+	$('fieldset.active').removeClass('active').prev('fieldset').addClass('active');
+})
 // END: multi form
-
 // START: typed
 let typer = new Typed('.typer', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 30,
-    loop: true,
-    startDelay: 1000,
-    backDelay: 2000
+	stringsElement: '#typed-strings',
+	typeSpeed: 30,
+	loop: true,
+	startDelay: 1000,
+	backDelay: 2000
 });
 // END: typed
-
 // START: foundation
 $(document).foundation()
 // END: foundation
-
 // START: form submit message (when valid)
-$('#contact-form').ajaxForm(function() { 
-  $('#contact-form').clearForm();
-  $("#callout").removeClass("hide");
-}); 
+$('#contact-form').ajaxForm(function() {
+	$('#contact-form').clearForm();
+	$("#callout").removeClass("hide");
+});
 // END: form submit message
